3849eeab046fd57af9ffe9dc335b6a86fbcbe43e
khushikumari128397-byte <khushikumari.128397@marwadiuniversity.ac.in>
2026-06-30 08:43:52 +0530

    Add files via upload

---
+++ EX7.php
@@ -0,0 +1,15 @@
+<?php
+require("header.php");
+?>
+
+<h2 align="center">
+    This content is written in index.php file.
+</h2>
+
+<p align="center">
+    PHP include and require function example.
+</p>
+
+<?php
+include("footer.php");
+?>
